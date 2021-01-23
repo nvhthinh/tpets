8aeee663b5608c95ff84d63a77b8296f455a668a
hoanthinh <thinhngo838@gmail.com>
2021-01-23 23:48:55 +0700

    item header

--- src/component/common/form/header_item.js
+++ src/component/common/form/header_item.js
@@ -8,12 +8,12 @@ export default class Login extends Component {
             <div className="row div_title_slide">
                 {/* header 2, menu*/}
                 <div className="col-lg-12 head_3 bg_div">
-                <nav id="nav_head" className="nav container nav-pills nav-fill">
-                    <a className="nav-item nav-link" href="#">SHOP CHO CÚN</a>
-                    <a className="nav-item nav-link" href="#">SHOP CHO MÈO</a>
-                    <a className="nav-item nav-link" href="#">KHUYẾN MẠI</a>
-                    <a className="nav-item nav-link" href="#">TIN TỨC</a>
-                    <a className="nav-item nav-link" href="#">LIÊN HỆ</a>
+                <nav id="nav_head_3" className="nav flex-column container">
+                    <Link to="/shopcun" className="nav-item nav-link" href="#"><span className="desk">SHOP CHO CÚN</span><i style={{display: 'none'}} className="fas fa-dog fa-2x mb" /></Link>
+                    <Link to="/shopmeo" className="nav-item nav-link" href="#"><span className="desk">SHOP CHO MÈO</span><i style={{display: 'none'}} className="fas fa-cat fa-2x mb" /></Link>
+                    <Link to="/giamgia" className="nav-item nav-link" href="#"><span className="desk">KHUYẾN MẠI</span><i style={{display: 'none'}} className="fas fa-percent fa-2x mb" /></Link>
+                    <Link to="/tintuc" className="nav-item nav-link" href="#"><span className="desk">TIN TỨC</span><i style={{display: 'none'}} className="fas fa-newspaper fa-2x mb" /></Link>
+                    <a className="nav-item nav-link" href="#"><span className="desk">LIÊN HỆ</span><i style={{display: 'none'}} className="fas fa-id-card-alt fa-2x mb" /></a>
                 </nav>
                 </div>
                 {/*end header 2, menu*/}
