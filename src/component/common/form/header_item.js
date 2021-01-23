75cba063bf29b85e4d9c2d4e90aef055e2c42a5c
hoanthinh <thinhngo838@gmail.com>
2021-01-23 23:53:15 +0700

    header2

--- src/component/common/form/header_item.js
+++ src/component/common/form/header_item.js
@@ -8,7 +8,7 @@ export default class Login extends Component {
             <div className="row div_title_slide">
                 {/* header 2, menu*/}
                 <div className="col-lg-12 head_3 bg_div">
-                <nav id="nav_head_3" className="nav flex-column container">
+                <nav id="nav_head" className="nav container nav-pills nav-fill">
                     <Link to="/shopcun" className="nav-item nav-link" href="#"><span className="desk">SHOP CHO CÚN</span><i style={{display: 'none'}} className="fas fa-dog fa-2x mb" /></Link>
                     <Link to="/shopmeo" className="nav-item nav-link" href="#"><span className="desk">SHOP CHO MÈO</span><i style={{display: 'none'}} className="fas fa-cat fa-2x mb" /></Link>
                     <Link to="/giamgia" className="nav-item nav-link" href="#"><span className="desk">KHUYẾN MẠI</span><i style={{display: 'none'}} className="fas fa-percent fa-2x mb" /></Link>
